9bc33c3fc50fac68cc690e4bba1b9942ee5a959d
Matvey26 <matvey.ivchenko@yandex.ru>
2023-12-12 18:05:52 +0300

    Небольшой рефакторинг

--- Autopilot/main.py
+++ Autopilot/main.py
@@ -11,13 +11,14 @@ ap = vessel.auto_pilot  # работать с автопилотом
 ap.target_pitch_and_heading(90, 90)
 ap.engage()
 
-stage_5_resources = vessel.resources_in_decouple_stage(stage=5, cumulative=False)
-ut = conn.add_stream(getattr, conn.space_center, 'ut')
-srb_fuel = conn.add_stream(stage_5_resources.amount, 'SolidFuel')  
-altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
-apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
-periapsis = conn.add_stream(getattr, vessel.orbit, 'periapsis_altitude')
-pitch = conn.add_stream(getattr, vessel.flight(), 'pitch')
+# переменные потоки, при вызове которых мы получаем данные из KSP
+ut = conn.add_stream(getattr, conn.space_center, 'ut')  # текущее время в KSP
+stage_5_resources = vessel.resources_in_decouple_stage(stage=5, cumulative=False)  # пятая ступень (та, где отделяются ускорители)
+srb_fuel = conn.add_stream(stage_5_resources.amount, 'SolidFuel')  # количество топлива во всех ускорителях в сумме
+altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')  # высота над уровнем моря в метрах
+apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')  # высота апоцентра в метрах, если считать от уровня моря
+periapsis = conn.add_stream(getattr, vessel.orbit, 'periapsis_altitude')  # высота перицентра в метрах, если счтиать от уровня моря
+pitch = conn.add_stream(getattr, vessel.flight(), 'pitch')  # рысканье ракеты
 
 
 # запускаем ракету
@@ -31,46 +32,67 @@ time.sleep(0.5)
 print("Start!")
 control.activate_next_stage()
 
+
+# параметры, с которыми ракета будет наклоняться в виде (высота апоцентра, угол рысканья)
+# первая точка, это какой угол должен быть в начале его изменения и на какой высоте апоцентра начать его менять
+# вторая точка, это значение угла в конце его изменения и высота апоцентра, на котором угол должен быть достигнут
 pos1, pos2 = (50_000, 90), (100_000, 0)
 k = (pos2[1] - pos1[1]) / (pos2[0] - pos1[0])
 b = pos1[1] - pos1[0] * k
 
 
+# сама функция угла
 def angle(ap):
-  return k * ap + b
+  return min(max(k * ap + b, 0), pos2[1])
 
 
-print(angle(50_000), angle(100_000))
-
+"""
+ВЗЛЁТ РАКЕТЫ (доорбитальный этап)
+Задачи:
+    1. запустить ракету
+    2. плавно лечь в горизонт
+    3. отбросить ускорители
+"""
+# ждём, пока апоцентр достигнет нужной высоты
 while apoapsis() < pos1[0]:
    time.sleep(0.5)  
 
+# наклоняем ракету до тех пор, пока топливо в тту не закончится
 while srb_fuel() >= 0.01:
     print(apoapsis(), angle(apoapsis()))
     ap.target_pitch = angle(apoapsis())
     time.sleep(0.2)
+ap.target_pitch = 0
 
+# отсоединаяем их
 control.activate_next_stage()
+print("Ускорители отброшены")
 
-# Мы вылетели из атмосферы кербина (NEXT STEP)
 
+
+
+"""
+ВЫХОД НА КРУГОВУЮ ОРБИТУ (орбитальный этап)
+Задачи:
+    1. Открыть солнечные панели
+    2. Набрать необходимую дельта скорость
+"""
 # Получение конкертной антенны и солнечной панели
 antennas = vessel.parts.antennas
 solar_pannels = vessel.parts.solar_panels
 
-# Включение
+# Раскрываем антенны 
 for a in antennas:
     if a.deployable:
         a.deployed = True
 
+# Раскрываем панели
 for s in solar_pannels:
     if s.deployable:
         s.deployed = True
 
-print("автопилот установлен на pitch = 0...")
-ap.target_pitch = 0
 
-
+# Функция, которая считает гомановский переход
 def test3(mu, r1, r2):
     # принимает стандартный гравитационный параметр mu
     # радиус круговой орбиты r1, радиус круговой орбиты r2
@@ -83,15 +105,12 @@ def test3(mu, r1, r2):
     return dv1, dv2
 
 
-# Planning circularization burn (using vis-viva equation)
 print("Добавляем ноду манёвра")
 mu = vessel.orbit.body.gravitational_parameter
 delta_v = test3(mu, vessel.orbit.periapsis, vessel.orbit.apoapsis)[1]
 node = control.add_node(ut() + vessel.orbit.time_to_apoapsis, prograde=delta_v)
-
 time.sleep(1)
 
-# Calculating burn time (using rocket equation)
 print("Считаем время работы двигателя")
 F = vessel.available_thrust
 Isp = vessel.specific_impulse * 9.82
@@ -99,24 +118,18 @@ m0 = vessel.mass
 m1 = m0 / math.exp(delta_v / Isp)
 flow_rate = F / Isp
 burn_time = (m0 - m1) / flow_rate
- 
-# Orientating the ship
-ap.disengage()
-print("Выключаем автопилот")
-control.sas = True
-print("Включаем САС")
-time.sleep(1)
-control.sas_mode = conn.space_center.SASMode.maneuver
-print("Ставим САС на манёвр")
 
-# Waiting until burn
-print("Ускоряем время до момента, когда мы ускоряться будем")
+print("Выключаем автопилот")
+ap.disengage()
+print("Включаем САС")
+control.sas = True
+time.sleep(1)
+print("Ставим САС на манёвр")
+control.sas_mode = conn.space_center.SASMode.maneuver
+
+print("Ждём момента до ускорения")
 burn_ut = ut() + vessel.orbit.time_to_apoapsis - (burn_time/2)
 lead_time = 5
-# conn.space_center.warp_to(burn_ut - lead_time)
-
-# Executing burn
-print("Ща запустим двигатели")
 time_to_apoapsis = conn.add_stream(getattr, vessel.orbit, "time_to_apoapsis")
 while time_to_apoapsis() - (burn_time / 2) > 0:
 	time.sleep(0.05)
@@ -127,6 +140,7 @@ time_when_end = ut() + vessel.orbit.time_to_apoapsis + burn_time / 2
 print(time_when_end - ut())
 while time_when_end - ut() > 0:
     time.sleep(0.05)
-print("The vessel is successfully parked at 100km orbit")
+
+print("Ракета успешно выведена на орбиту 100 км")
 control.throttle = 0
 control.remove_nodes()
